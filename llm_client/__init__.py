ea11e7c9409ccabcf8080d892b35b97643370d92
Giacomo Pastore <giacomo.pastore@gmail.com>
2024-11-09 17:42:53 +0100

    No AzureOpenAI

--- llm_client/__init__.py
+++ llm_client/__init__.py
@@ -1,6 +1,5 @@
 # llm_client/__init__.py
 
 from .ollama_client import OllamaClient, OllamaAsyncClient
-from .azure_openai_client import AzureOpenAIClient, AzureOpenAIAsyncClient
 
-__all__ = ["OllamaClient", "OllamaAsyncClient", "AzureOpenAIClient", "AzureOpenAIAsyncClient"]
+__all__ = ["OllamaClient", "OllamaAsyncClient"]
